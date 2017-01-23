d6c444adc119833d70779b21bded802b1fbb7cb4
Maggie Harrington and Patrick McGreevy <maggie@>
2017-01-23 14:56:27 -0800

    Add interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,34 @@ Task.prototype.priorityString = function() {
   else
     throw new Error("Invalid value for 'priority' property.");
 };
+
+// Interface logic
+$(document).ready(function() {
+  $("form#new-task").submit(function(event) {
+    event.preventDefault();
+
+    var title, due, notes, priority;
+    title = $("#new-title").val();
+    due = new Date($("#new-due").val());
+    notes = $("#new-notes").val();
+    priority = parseInt($("#new-priority").val());
+
+    var newTask = new Task(title, due, notes, priority);
+    tasks.push(newTask);
+
+    $("#tasks ul").append("<li><span class='task'>" + newTask.title + "</span></li>");
+    $("#tasks ul").last().click(function() {
+      $("#show-title").text(newTask.title);
+      $("#show-due").text(newTask.due.toDateString());
+      $("#show-notes").text(newTask.notes);
+      $("#show-priority").text(newTask.priorityString());
+
+      $("#show-task").show();
+    });
+
+    $("#new-title").val("");
+    $("#new-due").val("");
+    $("#new-notes").val("");
+    $("#new-priority").val("2");
+  });
+});
